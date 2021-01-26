8173c35d4d73d81f3bcf5a8702a2eedbee591103
Filipe <filipe14mattos@gmail.com>
2021-01-26 12:16:21 -0300

    finished the static header and footer

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+if(window.SimpleSlide){
+  new SimpleSlide({
+    slide: 'quote', // nome do atributo data-slide="principal"
+    time: 5000  // tempo de transição dos slides
+  });
+
+  new SimpleSlide({
+    slide: 'portifolio', // nome do atributo data-slide="principal"
+    time: 5000, // tempo de transição dos slides
+    nav: true  // abiliata a navegação
+  });
+}
+
+if(window.SimpleAnime){
+  new SimpleAnime();
+}
